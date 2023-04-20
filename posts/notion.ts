e7a4f6e254eb52e38b511d609eb5da8c3de78e32
anand-epi <anand.pandey803@gmail.com>
2023-04-20 23:15:04 +0530

    education and img added

--- posts/notion.ts
+++ posts/notion.ts
@@ -2,18 +2,15 @@ import { ExtendedRecordMap } from 'notion-types';
 import { getPageTitle } from 'notion-utils';
 
 export const POSTS = {
-  'go-graphql-api': {
-    date: new Date('2021-06-25').toDateString(),
-    uri: 'shblog/Production-ready-API-with-Go-and-GraphQL-4ebc1ab551e447c2ac14261a61da6032',
-  },
-  'go-dokku-deployment': {
+  'test-test': {
     date: new Date('2021-06-28').toDateString(),
     uri: 'https://anandpandey.notion.site/Anand-Pandey-s-Website-44775db0667a476388c81aa78364487e',
   },
-  'styled-system': {
-    date: new Date('2021-07-06').toDateString(),
-    uri: 'shblog/Rapid-UI-development-with-styled-system-e1ce2f8a2e64485491b49e4e3a6a8f96',
+  'test-test2': {
+    date: new Date('2021-06-28').toDateString(),
+    uri: 'https://anandpandey.notion.site/Anand-Pandey-s-Website-44775db0667a476388c81aa78364487e',
   },
+  
 };
 
 export interface PageInfo {
